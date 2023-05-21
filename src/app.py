7c7b97cb8396edbad1547e4cb41349c74a406857
Eli Moshe <moshebogo@gmail.com>
2023-05-20 23:54:49 -0400

    stuff

--- src/app.py
+++ src/app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, make_response, jsonify
 from flask_cors import CORS
 import pyautogui
 import subprocess
+from subprocess import Popen, PIPE
 import time
 
 app = Flask(__name__)
@@ -16,16 +17,25 @@ def test():
 @app.route("/command")
 def command():
     # this opens a new ubuntu terminal
-    subprocess.call(['gnome-terminal'])
-    pyautogui.hotkey('win', 'r')
-    time.sleep(4)
+    # subprocess.call(['gnome-terminal'])
+    # time.sleep(3)
+    run_drone = ['cd ../../dk ; python connection_template.py']
+    p = Popen(run_drone, stdout=PIPE, stderr=PIPE, shell=True)
+    stdout, stderr = p.communicate()
+    print(stdout)
+    print(stderr)
+    print(p.returncode)
+
+    # pyautogui.hotkey('win', 'r')
     # pyautogui.typewrite('ubuntu')
     # pyautogui.press('enter')
+
+
     # once a new ubuntu termina is open, it runs the code for the SITL drone
-    result = subprocess.run('cd ../../dk/ ; python connection_template.py' , capture_output=True, shell=True)
-    output = result.stdout.decode()
-    print(output)
-    return make_response(jsonify({'the output is:': output}))
+    # result = subprocess.run('cd ../../dk/ ; python connection_template.py' , capture_output=True, shell=True)
+    # output = result.stdout.decode()
+    # print(output)
+    return make_response(jsonify({'the output is:': 'output'}))
 
 
 
